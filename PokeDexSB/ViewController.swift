217d11abf1ee7ef2671f4e7752cc56a088de4969
Steven Ha <stevendha@gmail.com>
2019-12-07 16:31:45 -0700

    Initial Commit

---
+++ PokeDexSB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeDexSB
+//
+//  Created by steven ha on 12/7/19.
+//  Copyright © 2019 steven ha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
