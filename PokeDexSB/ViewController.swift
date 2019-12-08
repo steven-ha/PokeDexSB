f10590c4cb46c44c8366d708489550aec131c2e6
Steven Ha <stevendha@gmail.com>
2019-12-08 09:37:11 -0700

    Background color changed to yellow.

--- PokeDexSB/ViewController.swift
+++ PokeDexSB/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .yellow
         // Do any additional setup after loading the view.
     }
 
